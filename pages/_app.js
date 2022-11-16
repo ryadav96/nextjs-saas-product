6feee0ac01a1bdeef3f56f72725af9ca5231a32c
Rakesh Yadav <rakeshyadavnita@gmail.com>
2022-11-17 02:11:53 +0530

    ADDED CHAKRA UI and did cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
+import { ChakraProvider, CSSReset, ThemeProvider } from "@chakra-ui/react";
 import { ProvideAuth } from "../lib/auth";
 import "../styles/globals.css";
+import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ProvideAuth>
-      <Component {...pageProps} />
-    </ProvideAuth>
+    <ChakraProvider>
+      <ThemeProvider theme={theme}>
+        <ProvideAuth>
+          <CSSReset/>
+          <Component {...pageProps} />
+        </ProvideAuth>
+      </ThemeProvider>
+    </ChakraProvider>
   );
 }
 
